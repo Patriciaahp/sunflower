41ebb04e5012f5be7320a3d0e6f31181888e1a13
Patricia <patrihernandezperez9@gmail.com>
2024-11-07 08:45:57 +0100

    sunflower movile

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+for (var i = 0; i < 145; i++) {
+    document.write('<span class="rotated">Te quiero infinito </span>');
+
+}
+alert('¿Sabes?');
+alert('¿que?');
+alert('¿te?');
+alert('¿quiero?');
